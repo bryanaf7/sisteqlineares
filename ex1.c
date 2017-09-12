12b099057d734e2c512afd0e69eaf3647975aa0d
bryanaf7 <bryan.af7@gmail.com>
2017-09-12 13:48:15 -0300

    Pivoteamento

--- ex1.c
+++ ex1.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
 #define n 4
 
@@ -10,6 +12,39 @@ void somarl(float m[][n+1],int p,int i,float lambda)
 	return;
 }
 
+
+
+void trocar(float m[][n+1],int a,int b)
+{float aux;
+int j;
+
+	for(j=0;j<n+1;j++)
+	{aux=m[a][j];
+	m[a][j]=m[b][j];
+	m[b][j]=aux;}
+}
+
+
+void pivoteamento(float m[][n+1],int p)
+{int i,max;
+max=p;
+
+
+
+
+	for(i=p+1;i<n;i++)
+	{
+	if(fabs(m[i][p]))>fabs(m[max][p]))
+	m=i;
+	}
+
+trocar(m,p,max);
+return;
+}
+
+
+
+
 void gaussjordan(float m[][n+1],float x[])
 {float lambda;
 int i,p,j=0;
@@ -20,15 +55,16 @@ int i,p,j=0;
 
 	//MATRIZ TRIANGULAR
 	for(p=0;p<n-1;p++){
-	
-	//printf("\nok%d",p);
+
+	pivoteamento(m,p);
 	
 		for(i=p+1;i<n;i++){
 		lambda=m[i][p]/m[p][p];
 		somarl(m,p,i,lambda);
 		}
 	}
-
+	
+	//printando matriz triangular
 	for(i=0;i<4;i++){
 		for(j=0;j<5;j++)
 		printf("%f\t",m[i][j]);
